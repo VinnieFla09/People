f00393d16a87d4a5ff41fef08bf94cf446124a03
Vinny Flaherty <v.flaherty1@gmail.com>
2019-01-28 12:27:57 +0000

    VF web app 1 28.01.2019

--- People/Startup.cs
+++ People/Startup.cs
@@ -24,6 +24,7 @@ namespace People
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
