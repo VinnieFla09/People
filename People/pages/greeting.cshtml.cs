07fb60067c9780637b7b520cbf39fc6738dedc64
Vinny Flaherty <v.flaherty1@gmail.com>
2019-01-28 12:31:59 +0000

    VF finished 2nd worksheet 28.01.2019

---
+++ People/pages/greeting.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace People.pages
+{
+    public class greetingModel : PageModel
+    {
+        public string Message { get; set; } = "Hello there";
+
+        public void OnGet()
+        {
+          if (DateTime.Now.Hour >= 12)
+            {
+                Message = "good afternoon!!";
+            }
+
+          else
+            {
+                Message = "good morning";
+            }
+
+        }
+    }
+}
