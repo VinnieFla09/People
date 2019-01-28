39344e306ec1221d5ce1699c5315c1fdd41f8b45
Vinny Flaherty <v.flaherty1@gmail.com>
2019-01-28 18:49:00 +0000

    Added loop to list the 6 Nations participants in  a unordered list

---
+++ People/pages/W2ws1.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace People.pages
+{
+    public class W2ws1Model : PageModel
+    {
+        public string[] myArray { get; set; } = { "Ireland", "England", "France", "Wales", "Scotland", "Italy" };
+
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            
+
+        }
+    }
+}
