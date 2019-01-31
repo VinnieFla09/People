0fcccb6624d016d19f23d935d50b2dd5a1fd1cfd
Vinny Flaherty <v.flaherty1@gmail.com>
2019-01-31 20:51:27 +0000

    Added Taghelpplay cshtml and model

---
+++ People/pages/taghelpplay.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace People.pages
+{
+    public class taghelpplayModel : PageModel
+    {
+        [BindProperty]
+        public int Number1 { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
