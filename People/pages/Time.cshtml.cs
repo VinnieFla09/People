327fd6e396111b6974c84a6606af42f5f0c97f8b
Vinny Flaherty <v.flaherty1@gmail.com>
2019-02-06 09:02:25 +0000

    Completed Task 2

--- People/pages/Time.cshtml.cs
+++ People/pages/Time.cshtml.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using People.Models;
 
 namespace People.pages
 {
     public class TimeModel : PageModel
     {
-//Task 1
+        //Task 1
         public string[] DaysOfWeek { get; set; }
         = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 
@@ -25,8 +27,44 @@ namespace People.pages
         //Task2 
         //Create an object of the SelectMonth Class for each month (not chosen by default)
         [BindProperty]
+        public SelectMonths[] Months { get; set; }
+        =
+        {
+            new SelectMonths("Jan",false),
+            new SelectMonths("Feb",false),
+            new SelectMonths("Mar",false),
+            new SelectMonths("Apr",false),
+            new SelectMonths("May",false),
+            new SelectMonths("Jun",false),
+            new SelectMonths("Jul",false),
+            new SelectMonths("Aug",false),
+            new SelectMonths("Sept",false),
+            new SelectMonths("Oct",false),
+            new SelectMonths("Nov",false),
+            new SelectMonths("Dec",false),
+        };
 
-        
+        public List<SelectListItem> MonthList { get; set; } =
+            new List<SelectListItem>
+            {
+            new SelectListItem("Jan","Jan"),
+            new SelectListItem("Feb","Feb"),
+            new SelectListItem("Mar","Mar"),
+            new SelectListItem("Apr","Apr"),
+            new SelectListItem("May","May"),
+            new SelectListItem("Jun","Jun"),
+            new SelectListItem("Jul","Jul"),
+            new SelectListItem("Aug","Aug"),
+            new SelectListItem("Sept","Sept"),
+            new SelectListItem("Oct","Oct"),
+            new SelectListItem("Nov","Nov"),
+            new SelectListItem("Dec","Dec")
+            };
+
+        [BindProperty]
+        public string[] ChosenMonths { get; set; } = { };
+
+
 
         public void OnGet()
         {
