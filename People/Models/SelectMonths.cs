d107caee0c02c14b0749456b82c57675e518595d
Vinny Flaherty <v.flaherty1@gmail.com>
2019-02-06 07:47:42 +0000

    Completed task 1 on time.cshtml worksheert

---
+++ People/Models/SelectMonths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//This class holds the name of the month and if it is selected
+namespace People.Models
+{
+    public class SelectMonths
+    {
+        public string MonthName { get; set; }
+        public bool IsSelected { get; set; }
+
+        public SelectMonths(string monthName, bool isSelected)
+        {
+            MonthName = monthName;
+            IsSelected = isSelected;
+        }
+
+        public SelectMonths()
+        {
+            MonthName = "";
+            IsSelected = false;
+        }
+    }
+}
---
+++ People/pages/Time.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace People.pages
+{
+    public class TimeModel : PageModel
+    {
+//Task 1
+        public string[] DaysOfWeek { get; set; }
+        = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        [BindProperty]
+        public bool[] DaysSelected { get; set; }
+        = { false, false, false, false, false, false, false };
+
+        [BindProperty]
+        public string[] DaysToPrint { get; set; }
+
+        public bool Posted { get; set; } = false;
+
+        //Task2 
+        //Create an object of the SelectMonth Class for each month (not chosen by default)
+        [BindProperty]
+
+        
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            Posted = true;
+        }
+    }
+}
